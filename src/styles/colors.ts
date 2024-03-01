a1a75ea3c9004f1fc933e0e244270fb8c45e4cd5
Ubirajara Pelli <ubirajarapelli@brq.com>
2024-03-01 16:06:46 -0300

    feat: add font and colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,19 @@
+export const colors = {
+  primary: {
+    main: "#FFC00C",
+  },
+  secondary: {
+    main: "#B2AFA8",
+  },
+  text: {
+    main: "#3A3222",
+  },
+  actions: {
+    success: {
+      main: "#4CAF50",
+    },
+    danger: {
+      main: "#C45317",
+    },
+  },
+};
