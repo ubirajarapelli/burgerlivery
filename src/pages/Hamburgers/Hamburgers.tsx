a6ede1be2885ce185a1df934b9ede14d3863ecfd
Ubirajara Pelli <ubirajaracpelli@gmail.com>
2024-03-06 13:58:49 -0300

    feat: add navigate home and populate burger page

--- src/pages/Hamburgers/Hamburgers.tsx
+++ src/pages/Hamburgers/Hamburgers.tsx
@@ -1,12 +1,54 @@
-import { Layout } from "../../components";
+import { useEffect, useState } from "react";
+import { Button, CategoryList, Layout, ProductCard } from "../../components";
 import { ProductCategories, ProductWrapper } from "./Hamburgers.style";
+import { burgers, categoriesList } from "../../mocks";
+import {
+  ProductCardContent,
+  ProductCardPrice,
+} from "../../components/ProductCard/ProductCard.style";
 
 export default function Hamburgers() {
+  const [categories, setCategories] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  const priceFormat = (price: number) => {
+    return new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    }).format(price);
+  };
+
+  useEffect(() => {
+    setCategories(categoriesList);
+  }, []);
+
+  useEffect(() => {
+    setProducts(burgers);
+  }, []);
+
   return (
     <Layout>
       <h1>Hamburgers</h1>
-      <ProductCategories></ProductCategories>
-      <ProductWrapper></ProductWrapper>
+      <ProductCategories>
+        {categories.map((item, index) => (
+          <CategoryList key={index} data={item} />
+        ))}
+      </ProductCategories>
+      <ProductWrapper>
+        {products.map((product, index) => (
+          <ProductCard key={index}>
+            <ProductCardContent>
+              <h2>{product.title}</h2>
+              <p>{product.description}</p>
+              <Button onClick={() => {}}>Adicionar</Button>
+            </ProductCardContent>
+            <ProductCardPrice>
+              {priceFormat(product.values.single)}
+            </ProductCardPrice>
+            <img src={product.image[0]} alt={product.title} />
+          </ProductCard>
+        ))}
+      </ProductWrapper>
     </Layout>
   );
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,15 +5,22 @@ import {
   HomeWrapperElement,
 } from "./Home.style";
 import HomeBg from "../../assets/bg-top.jpg";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/hamburgers");
+  };
+
   return (
     <Layout>
       <HomeWrapperElement>
         <HomeActionContentElement>
           <h1>Hamburgers para todos os gostos</h1>
           <p>Nós fazemos deliciosos hamburgers e entregamos na sua porta</p>
-          <Button onClick={() => {}}>Peça agora</Button>
+          <Button onClick={handleClick}>Peça agora</Button>
         </HomeActionContentElement>
         <HomeBgImage src={HomeBg} alt="placeholder" />
       </HomeWrapperElement>
